bc0ce3f2a27a5228be241b9f2ce87bd5f6cbee7a
shinhyo <wonshinhyo@gmail.com>
2022-03-31 18:14:51 +0900

    (DP-115) 샘플 패키지 네임 변경

--- sample/src/main/java/com/wanted/android/designsystem/sample/MainActivity.kt
+++ sample/src/main/java/com/wanted/android/designsystem/sample/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.wanted.android.designsystem
+package com.wanted.android.designsystem.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
