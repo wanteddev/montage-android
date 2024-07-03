184e14020818c46ab634a84b936c74d47d3e28fc
myseong <myeongyun.seong@wantedlab.com>
2024-07-03 13:53:24 +0900

    PI-65607[Android][엔지니어링위크] Material3로 교체

--- src/main/java/com/wanted/android/wanted/design/dialog/WantedDialog.kt
+++ src/main/java/com/wanted/android/wanted/design/dialog/WantedDialog.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
--- src/main/java/com/wanted/android/wanted/design/dialog/view/WantedDialogTwoButtonImpl.kt
+++ src/main/java/com/wanted/android/wanted/design/dialog/view/WantedDialogTwoButtonImpl.kt
@@ -12,8 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
