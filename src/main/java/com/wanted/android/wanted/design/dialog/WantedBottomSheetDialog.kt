a8f28a9a0b900614f3388564ba8c1be6a2c8e5f4
myseong <myeongyun.seong@wantedlab.com>
2024-08-07 16:06:32 +0900

    BottomSheetDialog 기본 base 코드 생성

---
+++ src/main/java/com/wanted/android/wanted/design/dialog/WantedBottomSheetDialog.kt
@@ -0,0 +1,174 @@
+package com.wanted.android.wanted.design.dialog
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.view.Gravity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.FrameLayout
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
+import com.wanted.android.designsystem.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun WantedBottomSheetDialog(
+    isVisible: Boolean,
+    durationMillis: Long = 200,
+    onDismissRequest: () -> Unit = {},
+    top: @Composable () -> Unit,
+    bottom: @Composable (() -> Unit)? = null,
+    content: @Composable () -> Unit
+) {
+    val visibleState: MutableTransitionState<Boolean> = remember { MutableTransitionState(false) }
+    val dialogVisibility: MutableState<Boolean> = remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = isVisible) {
+        if (isVisible) {
+            dialogVisibility.value = true
+            visibleState.targetState = true
+        } else if (visibleState.currentState) {
+            visibleState.targetState = false
+
+            launch {
+                delay(durationMillis)
+                dialogVisibility.value = false
+                onDismissRequest()
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = visibleState.currentState) {
+        if (!isVisible) {
+            visibleState.targetState = false
+            dialogVisibility.value = false
+        }
+    }
+
+    if (dialogVisibility.value) {
+        Dialog(
+            properties = DialogProperties(
+                usePlatformDefaultWidth = false,
+                decorFitsSystemWindows = false,
+            ),
+            onDismissRequest = {
+                onDismissRequest()
+            }
+        ) {
+            SetDialogDestinationToEdgeToEdge()
+
+            AnimatedVisibility(
+                visibleState = visibleState,
+                enter = slideInVertically() + expandVertically(
+                    expandFrom = Alignment.Top
+                ) + fadeIn(
+                    initialAlpha = 0.3f
+                ),
+                exit = slideOutVertically { 0 } + shrinkVertically(
+                    shrinkTowards = Alignment.Top
+                ) + fadeOut()
+            ) {
+                WantedBottomSheetLayout(
+                    top = top,
+                    bottom = bottom,
+                    content = content
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun WantedBottomSheetLayout(
+    modifier: Modifier = Modifier,
+    top: @Composable () -> Unit,
+    bottom: @Composable (() -> Unit)?,
+    content: @Composable () -> Unit
+) {
+
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+        shadowElevation = 4.dp,
+        color = colorResource(id = R.color.background_normal_normal)
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            top()
+
+            content()
+
+            bottom?.invoke()
+
+            Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.systemBars))
+        }
+    }
+}
+
+@Composable
+fun getActivityWindow(): Window? = LocalView.current.context.getActivityWindow()
+
+private tailrec fun Context.getActivityWindow(): Window? =
+    when (this) {
+        is Activity -> window
+        is ContextWrapper -> baseContext.getActivityWindow()
+        else -> null
+    }
+
+@Composable
+fun SetDialogDestinationToEdgeToEdge(
+    gravity: Int = Gravity.BOTTOM,
+    dimAmount: Float = 0.52f
+) {
+    val activityWindow = getActivityWindow()
+    val dialogWindow = (LocalView.current.parent as? DialogWindowProvider)?.window
+    val parentView = LocalView.current.parent as View
+    SideEffect {
+        if (activityWindow != null && dialogWindow != null) {
+            val attributes = WindowManager.LayoutParams()
+            attributes.copyFrom(activityWindow.attributes)
+            attributes.dimAmount = dimAmount
+            attributes.type = dialogWindow.attributes.type
+            attributes.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND
+            dialogWindow.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+            dialogWindow.attributes = attributes
+            dialogWindow.setDimAmount(dimAmount)
+            dialogWindow.setGravity(gravity)
+            parentView.layoutParams = FrameLayout.LayoutParams(
+                activityWindow.decorView.width,
+                activityWindow.decorView.height
+            )
+        }
+    }
+}
