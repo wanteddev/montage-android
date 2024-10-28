69443442acbcf729833566d38e3b403b8bde4b43
nayunjae <137739236+nayunjae@users.noreply.github.com>
2024-10-28 20:25:45 +0900

    허수 지원 1차 (#4429)
    * (PI-68718) [Android] 동시지원 제안 화면 개선
    
    * (PI-68970)[Android] 허수지원 감소 프로젝트 1차 이벤트 작업 (#4422)
    
    * PI-68970 (#4423)
    
    * (PI-68970)[Android] 허수지원 감소 프로젝트 1차 이벤트 작업
    
    * (PI-68970)[Android] 허수지원 감소 프로젝트 1차 이벤트 작업
    
    * 테스트 수정
    
    * ActionArea 수정 (#4424)
    
    * 이벤트 수정
    
    * (PI-69037)[Android] 동시지원 리스트 디자인 이슈
    
    * (PI-69037)[Android] 동시지원 리스트 디자인 이슈
    
    * bottom 영역 위로 Content가 올라가도록 수정
    
    * (PI-69037)[Android] 동시지원 리스트 디자인 이슈
    
    * (PI-69037)[Android] 동시지원 리스트 디자인 이슈
    
    * 동시지원 오류 수정
    
    * 빌드 에러 수정
    
    ---------
    
    Co-authored-by: myseong-wanted <116132369+myseong-wanted@users.noreply.github.com>
    Co-authored-by: danchoo21 <danchoo45@naver.com>

---
+++ src/main/java/com/wanted/android/wanted/design/snackbar/WantedSnackBarHost.kt
@@ -0,0 +1,103 @@
+package com.wanted.android.wanted.design.snackbar
+
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import com.wanted.android.wanted.design.toast.WantedToast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun WantedSnackbarHost(
+    hostState: SnackbarHostState,
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+) {
+    SnackbarHost(
+        hostState = hostState,
+        snackbar = {
+            val visuals =
+                it.visuals as? WantedSnackbarVisuals ?: return@SnackbarHost
+            when (val type = visuals.snackbarType) {
+                is SnackbarType.Snackbar -> {
+                    WantedSnackBar(
+                        modifier = Modifier
+                            .padding(paddingValues = visuals.padding)
+                            .navigationBarsPadding()
+                            .imePadding(),
+                        text = visuals.message,
+                        snackbarData = it,
+                        description = type.description,
+                        buttonText = visuals.actionLabel ?: "",
+                        icon = type.icon,
+                        onClick = {}
+                    )
+                }
+
+                is SnackbarType.Toast -> {
+                    WantedToast(
+                        modifier = Modifier
+                            .padding(paddingValues = visuals.padding)
+                            .navigationBarsPadding()
+                            .imePadding(),
+                        text = visuals.message,
+                        variant = type.variant,
+                        icon = type.icon
+                    )
+                }
+            }
+        }
+    )
+
+
+    ObserveAsEvents(
+        flow = SnackbarController.events,
+        key1 = hostState
+    ) { event ->
+        coroutineScope.launch {
+            hostState.currentSnackbarData?.dismiss()
+            val result = hostState.showSnackbar(
+                WantedSnackbarVisuals(
+                    message = event.message,
+                    actionLabel = event.action?.name,
+                    duration = event.duration,
+                    withDismissAction = event.withDismissAction,
+                    snackbarType = event.snackbarType,
+                    padding = event.padding
+                )
+            )
+            if (result == SnackbarResult.ActionPerformed) {
+                event.action?.action?.invoke()
+            }
+        }
+    }
+}
+
+@Composable
+private fun <T> ObserveAsEvents(
+    flow: Flow<T>,
+    key1: Any? = null,
+    key2: Any? = null,
+    onEvent: (T) -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(lifecycleOwner.lifecycle, key1, key2, flow) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect(onEvent)
+            }
+        }
+    }
+}
