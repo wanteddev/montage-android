b007001c674d409630e5342c551d6fb43284e8e7
ggong <ggong@gongdaecheol-ui-noteubug.local>
2024-11-03 00:18:13 +0900

    이력서 메모 기능 오프라인으로 완료

--- src/main/java/com/wanted/android/wanted/design/dialog/WantedDialog.kt
+++ src/main/java/com/wanted/android/wanted/design/dialog/WantedDialog.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -119,6 +121,34 @@ private fun WantedAlertDialogLayout(
             negative?.invoke()
 
             positive()
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun WantedDialogPreview() {
+    DesignSystemTheme {
+        Surface {
+            Column {
+                WantedDialog(
+                    title = "다이얼로그 타이틀",
+                    message = "다이얼로그 메시지",
+                    confirm = "확인",
+                    cancel = "취소",
+                    onClickConfirm = {},
+                    onClickCancel = {}
+                )
+
+                WantedDialog(
+                    title = "다이얼로그 타이틀이 두줄이 되는 경우에는 어떻게 될까요?",
+                    message = "다이얼로그 메시지",
+                    confirm = "확인",
+                    cancel = "취소",
+                    onClickConfirm = {},
+                    onClickCancel = {}
+                )
+            }
         }
     }
 }
