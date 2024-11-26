c3bd422308150994200de81f5432602d0874ca00
nayunjae <137739236+nayunjae@users.noreply.github.com>
2024-11-26 21:59:25 +0900

    허수 지원 2차 (#4484)
    * card layout 구상
    
    * card 일부 업데이트
    
    * card
    
    * card
    
    * 디자인 수정사항 반영
    
    * card Skeleton
    
    * WantedCard Touch 영역 확장
    
    * PI-69562[Android] Cloud Front 적용 테스트
    
    * version update 13.11.0
    
    * 클라우드 프론트 적용
    
    * (PI-69529)[Android] 공고 상세 AI 스코어 영역 개발 (#4470)
    
    * (PI-69529)[Android] 공고 상세 AI 스코어 영역 개발
    
    * (PI-69529)[Android] 공고 상세 AI 스코어 영역 개발
    
    Unused import 제거
    
    * (PI-69677)[Android] 허수 지원 2차 테스트 코드 추가 (#4473)
    
    * (PI-69677)[Android] 허수 지원 2차 테스트 코드 추가
    
    * 문구, 이미지 수정
    
    * (PI-69758[Android] 연차 부적합 > AI 점수 영역 "다른포지션에 지원해 보세요" 가 아닌 다른 텍스트 노출 됨 (#4475)
    
    * 허수 지원 2차 design qa (#4483)
    
    * (PI-69870)[Android] 직무, 연차 부적합 케이스 디자인 이슈
    
    * (PI-69872)[Android] 이력서 작성 완료 > 모달 디자인 이슈
    
    * (PI-69871)[Android] 직무 부적합 +0.5 이상 케이스 디자인 이슈
    
    * 번역키 업데이트
    
    * (PI-69870)[Android] 직무, 연차 부적합 케이스 디자인 이슈
    
    * (PI-69939)[Android] /api/v4/profile/{user_hash}/level2 api를 파이썬3 API로 마이그레이션 (#4487)
    
    * 허수 지원 이벤트 추가 (임시) (#4488)
    
    ---------
    
    Co-authored-by: danchoo21 <danchoo45@naver.com>
    Co-authored-by: myseong <myeongyun.seong@wantedlab.com>
    Co-authored-by: myseong-wanted <116132369+myseong-wanted@users.noreply.github.com>

---
+++ src/main/java/com/wanted/android/wanted/design/card/WantedCardDescription.kt
@@ -0,0 +1,228 @@
+package com.wanted.android.wanted.design.card
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wanted.android.designsystem.R
+import com.wanted.android.wanted.design.badge.WantedContentBadge
+import com.wanted.android.wanted.design.loading.WantedSkeletonLength
+import com.wanted.android.wanted.design.loading.WantedSkeletonRectangle
+import com.wanted.android.wanted.design.loading.WantedSkeletonText
+import com.wanted.android.wanted.design.theme.DesignSystemTheme
+import com.wanted.android.wanted.design.util.WantedTextStyle
+
+
+@Composable
+fun WantedCardDescription(
+    modifier: Modifier = Modifier,
+    title: String,
+    maxLines: Int = 2,
+    caption: String = "",
+    extraCaption: String = "",
+    topContent: @Composable (() -> Unit)? = null,
+    bottomContent: @Composable (() -> Unit)? = null
+) {
+    WantedCardDescriptionLayout(
+        modifier = modifier,
+        topContent = topContent,
+        title = {
+            Text(
+                text = title,
+                maxLines = maxLines,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        cation = caption.ifEmpty { null }?.let {
+            {
+                Text(
+                    text = caption,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        },
+        extraCaption = extraCaption.ifEmpty { null }?.let {
+            {
+                Text(
+                    text = extraCaption,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        },
+        bottomContent = bottomContent
+    )
+}
+
+
+@Composable
+internal fun WantedCardDescriptionSkeleton(
+    modifier: Modifier = Modifier,
+    topContent: Boolean = false,
+    caption: Boolean = true,
+    extraCaption: Boolean = true,
+    bottomContent: Boolean = false
+) {
+    WantedCardDescriptionLayout(
+        modifier = modifier,
+        topContent = if (topContent) {
+            { WantedSkeletonRectangle(modifier = Modifier.size(48.dp, 20.dp)) }
+        } else null,
+        title = {
+            WantedSkeletonText(
+                modifier = Modifier.height(22.dp),
+                length = WantedSkeletonLength.Ratio100
+            )
+        },
+        cation = if (caption) {
+            {
+                WantedSkeletonText(
+                    modifier = Modifier.height(18.dp),
+                    length = WantedSkeletonLength.Ratio50
+                )
+            }
+        } else null,
+        extraCaption = if (extraCaption) {
+            {
+                WantedSkeletonText(
+                    modifier = Modifier.height(18.dp),
+                    length = WantedSkeletonLength.Ratio25
+                )
+            }
+        } else null,
+        bottomContent = if (bottomContent) {
+            { WantedSkeletonRectangle(modifier = Modifier.size(48.dp, 20.dp)) }
+        } else null
+    )
+}
+
+
+@Composable
+internal fun WantedCardDescriptionLayout(
+    modifier: Modifier = Modifier,
+    topContent: @Composable (() -> Unit)? = null,
+    title: @Composable () -> Unit,
+    cation: @Composable (() -> Unit)? = null,
+    extraCaption: @Composable (() -> Unit)? = null,
+    bottomContent: @Composable (() -> Unit)? = null
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        topContent?.let {
+            Box(modifier = Modifier.padding(bottom = 4.dp)) {
+                topContent()
+            }
+        }
+
+        ProvideTextStyle(
+            value = WantedTextStyle(
+                colorRes = R.color.label_normal,
+                style = DesignSystemTheme.typography.body1Bold
+            )
+        ) {
+            title()
+        }
+
+        ProvideTextStyle(
+            value = WantedTextStyle(
+                colorRes = R.color.label_alternative,
+                style = DesignSystemTheme.typography.label2Medium
+            )
+        ) {
+            cation?.invoke()
+
+            extraCaption?.invoke()
+        }
+
+        bottomContent?.let {
+            Box(modifier = Modifier.padding(top = 4.dp)) {
+                bottomContent()
+            }
+        }
+    }
+}
+
+
+@Preview("light", uiMode = Configuration.UI_MODE_NIGHT_NO, locale = "ko")
+@Preview("dark", uiMode = Configuration.UI_MODE_NIGHT_YES, locale = "ko")
+@Preview(
+    "foldableLight",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    locale = "ko",
+    device = Devices.FOLDABLE
+)
+@Composable
+private fun WantedCardDescriptionPreview() {
+    DesignSystemTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+            ) {
+                WantedCardDescription(
+                    modifier = Modifier,
+                    title = "제목"
+                )
+
+                WantedCardDescription(
+                    modifier = Modifier,
+                    title = "제목",
+                    caption = "캡션"
+                )
+
+                WantedCardDescription(
+                    modifier = Modifier,
+                    title = "제목",
+                    caption = "캡션",
+                    extraCaption = "추가 캡션"
+                )
+
+                WantedCardDescription(
+                    modifier = Modifier,
+                    title = "제목",
+                    caption = "캡션",
+                    extraCaption = "추가 캡션",
+                    topContent = {
+                        WantedContentBadge(text = "텍스트")
+                    }
+                )
+
+                WantedCardDescription(
+                    modifier = Modifier,
+                    title = "제목",
+                    caption = "캡션",
+                    extraCaption = "추가 캡션",
+                    bottomContent = {
+                        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+                            WantedContentBadge(text = "텍스트")
+                            WantedContentBadge(text = "텍스트")
+                        }
+                    }
+                )
+
+                WantedCardDescriptionSkeleton()
+
+                WantedCardDescriptionSkeleton(topContent = true, bottomContent = true)
+            }
+        }
+    }
+}
