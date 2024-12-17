29409d483806a5cce9b00583a6e865e19e7e2de5
myseong <myeongyun.seong@wantedlab.com>
2024-12-17 17:12:09 +0900

    PI-70502 [Android] Pagination 제작

--- src/main/java/com/wanted/android/wanted/design/pagination/WantedDotIndicator.kt
+++ src/main/java/com/wanted/android/wanted/design/pagination/WantedDotIndicator.kt
@@ -1,5 +1,8 @@
 package com.wanted.android.wanted.design.pagination
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -7,130 +10,257 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wanted.android.designsystem.R
 import com.wanted.android.wanted.design.DevicePreviews
+import com.wanted.android.wanted.design.pagination.WantedIndicatorContract.IndicatorDotSize
+import com.wanted.android.wanted.design.pagination.WantedIndicatorContract.WantedIndicatorSize
 import com.wanted.android.wanted.design.theme.DesignSystemTheme
 import com.wanted.android.wanted.design.util.OPACITY_43
 import com.wanted.android.wanted.design.util.OPACITY_8
+import kotlin.math.abs
+import kotlin.math.floor
 
-fun PagerState.calculateCurrentOffsetForPage(page: Int): Float {
-    return (currentPage - page) + currentPageOffsetFraction
-}
-
-fun PagerState.calculateCurrentOffsetForPage1(page: Int): Float {
-    return (currentPage - page) + currentPageOffsetFraction
-}
-
-
-private const val SELECT_SCALE = 1f
-private const val DESELECT_SCALE_80 = 0.8f
-private const val DESELECT_SCALE_60 = 0.6f
-//
-@Composable
-fun WantedDotIndicator(
-    modifier: Modifier = Modifier,
-    pageCount: Int,
-    pagerState: PagerState
-) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
-    ) {
-        repeat(pageCount) {
-            Box(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .size(8.dp)
-                    .background(
-                        color = if (pagerState.calculateCurrentOffsetForPage(it).toInt() == it) {
-                            colorResource(id = R.color.label_normal)
-                        } else {
-                            colorResource(id = R.color.label_normal).copy(OPACITY_43)
-                        },
-                        shape = CircleShape
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = colorResource(id = R.color.label_normal).copy(OPACITY_8),
-                        shape = CircleShape
-                    )
-            )
-        }
-    }
-}
 
 @Composable
 fun WantedDotIndicator(
     modifier: Modifier = Modifier,
-    visibleCount: Int,
+    indicatorSize: WantedIndicatorSize = WantedIndicatorSize.Normal,
+    totalPageCount: Int,
+    visibleDotCount: Int,
     currentIndex: Int
 ) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
-    ) {
-        repeat(visibleCount) {
-            Box(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .size(8.dp)
-                    .background(
-                        color = if (currentIndex == it) {
-                            colorResource(id = R.color.label_normal)
-                        } else {
-                            colorResource(id = R.color.label_normal).copy(OPACITY_43)
-                        },
-                        shape = CircleShape
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = colorResource(id = R.color.label_normal).copy(OPACITY_8),
-                        shape = CircleShape
-                    )
+    val visibleArea by remember(visibleDotCount, totalPageCount, currentIndex) {
+        mutableStateOf(
+            getPaginationDotVisibleArea(
+                maxDotCount = visibleDotCount,
+                totalPageCount = totalPageCount,
+                currentIndex = currentIndex
             )
+        )
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 10.dp,
+            alignment = Alignment.CenterHorizontally
+        )
+    ) {
+        repeat(totalPageCount) { index ->
+            IndicatorDot(
+                indicatorSize = indicatorSize,
+                index = index,
+                visibleArea = visibleArea,
+                visibleDotCount = visibleDotCount,
+                totalPageCount = totalPageCount,
+                currentIndex = currentIndex,
+
+                )
         }
     }
 }
 
-private fun getCurrentIndex(
-    totalCount:
+@Composable
+private fun IndicatorDot(
+    modifier: Modifier = Modifier,
+    indicatorSize: WantedIndicatorSize,
+    index: Int,
+    visibleArea: Pair<Int, Int>,
+    visibleDotCount: Int,
+    totalPageCount: Int,
+    currentIndex: Int
 ) {
 
-}
+    val indicatorDotSize by remember(
+        index,
+        visibleArea,
+        visibleDotCount,
+        totalPageCount,
+        currentIndex
+    ) {
+        mutableStateOf(
+            getDotSize(
+                size = indicatorSize,
+                dotSize = getIndicatorDotSize(
+                    index = index,
+                    visibleStartIndex = visibleArea.first,
+                    visibleEndIndex = visibleArea.second,
+                    visibleDotCount = visibleDotCount,
+                    totalPageCount = totalPageCount
+                )
+            )
+        )
+    }
 
-/**
- * 배너를 무한 스크롤 하기 위한 마지막 더미 페이지 인지 확인
- */
-private fun isAfterLastPage(itemCount: Int, currentPage: Int): Boolean {
-    return itemCount + 1 == currentPage
-}
+    val isVisible by remember(indicatorDotSize) { mutableStateOf(indicatorDotSize > 0.dp) }
 
-private fun isBeforeFistPage(currentPage: Int): Boolean {
-    return currentPage == 0
-}
-
-private fun getLastPageIndex(itemCount: Int): Int {
-    return itemCount - 1
-}
-@Composable
-private fun getCurrentIndex(itemCount: Int, currentPage: Int): Int {
-    return when {
-        isAfterLastPage(itemCount, currentPage) -> 0
-        isBeforeFistPage(currentPage) -> getLastPageIndex(itemCount)
-        else -> currentPage - 1
+    AnimatedVisibility(
+        modifier = modifier,
+        visible = isVisible,
+        enter = if (visibleArea.first == index) {
+            expandHorizontally(expandFrom = Alignment.End)
+        } else {
+            expandHorizontally(expandFrom = Alignment.Start)
+        },
+        exit = if (visibleArea.first == index) {
+            shrinkHorizontally(shrinkTowards = Alignment.End)
+        } else {
+            shrinkHorizontally(shrinkTowards = Alignment.Start)
+        }
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(indicatorDotSize)
+                .background(
+                    color = if (currentIndex == index) {
+                        colorResource(id = R.color.label_normal)
+                    } else {
+                        colorResource(id = R.color.label_normal).copy(OPACITY_43)
+                    },
+                    shape = CircleShape
+                )
+                .border(
+                    width = 1.dp,
+                    color = colorResource(id = R.color.label_normal).copy(OPACITY_8),
+                    shape = CircleShape
+                )
+        )
     }
 }
 
+fun getPaginationDotVisibleArea(
+    maxDotCount: Int = 5,
+    totalPageCount: Int = 10,
+    currentIndex: Int = 0
+): Pair<Int, Int> {
+    val centerIndex = floor(maxDotCount * 0.5).toInt()
+    val isEven = maxDotCount % 2 == 0
+
+    /**
+     * dot 이 짝수일때 Center
+     */
+    if (isEven
+        && currentIndex >= centerIndex
+        && totalPageCount - centerIndex > currentIndex
+    ) {
+        return Pair(currentIndex - centerIndex + 1, currentIndex + centerIndex)
+    }
+
+
+    /**
+     * 처음
+     */
+    if (currentIndex <= centerIndex) {
+        return Pair(0, maxDotCount - 1)
+    }
+
+    /**
+     * 마지막
+     */
+    if (totalPageCount - centerIndex <= currentIndex) {
+        return Pair(totalPageCount - maxDotCount, totalPageCount - 1)
+    }
+
+    /**
+     * 중간
+     */
+    return Pair(currentIndex - centerIndex, currentIndex + centerIndex)
+}
+
+private fun getDotSize(
+    size: WantedIndicatorSize,
+    dotSize: IndicatorDotSize
+): Dp {
+    return if (size == WantedIndicatorSize.Normal) {
+        when (dotSize) {
+            IndicatorDotSize.Max -> 10
+            IndicatorDotSize.Mid -> 8
+            IndicatorDotSize.Min -> 6
+            IndicatorDotSize.Zero -> 0
+        }
+
+    } else {
+        when (dotSize) {
+            IndicatorDotSize.Max -> 6
+            IndicatorDotSize.Mid -> 4
+            IndicatorDotSize.Min -> 2
+            IndicatorDotSize.Zero -> 0
+        }
+    }.dp
+}
+
+private fun getIndicatorDotSize(
+    index: Int,
+    visibleStartIndex: Int,
+    visibleEndIndex: Int,
+    visibleDotCount: Int,
+    totalPageCount: Int
+): IndicatorDotSize {
+
+    if (index < visibleStartIndex || index > visibleEndIndex) {
+        return IndicatorDotSize.Zero
+    }
+
+    val centerIndex = floor(visibleDotCount * 0.5)
+
+    // first
+    if ((visibleStartIndex == 0 && centerIndex > index)
+        || (visibleStartIndex == 1 && index == 2)
+    ) {
+        return IndicatorDotSize.Max
+    }
+
+    // last
+    if (
+        (visibleEndIndex == totalPageCount - 1 && index >= totalPageCount - centerIndex - 1)
+        || (visibleEndIndex == totalPageCount - 2 && index == totalPageCount - 3)
+    ) {
+        return IndicatorDotSize.Max
+    }
+
+    val distance = abs(index - visibleStartIndex).coerceAtMost(abs(index - visibleEndIndex))
+    if ((visibleStartIndex == 1 && index == 1)
+        || (visibleEndIndex == totalPageCount - 2 && index == totalPageCount - 2)
+    ) {
+        return IndicatorDotSize.Mid
+    }
+
+    return when (distance) {
+        1 -> {
+            if (visibleDotCount <= 4) {
+                IndicatorDotSize.Max
+            } else {
+                IndicatorDotSize.Mid
+            }
+        }
+
+        0 -> {
+            if (visibleDotCount <= 4) {
+                IndicatorDotSize.Mid
+            } else {
+                IndicatorDotSize.Min
+            }
+        }
+
+        else -> IndicatorDotSize.Max
+    }
+}
 
 @DevicePreviews
 @Composable
@@ -143,9 +273,12 @@ private fun WantedDotIndicatorPreview() {
                     .padding(20.dp),
                 verticalArrangement = Arrangement.spacedBy(20.dp),
             ) {
-//                WantedDotIndicator(
-//                    pagerState = rememberPagerState(initialPage = 0) { 10 }
-//                )
+                WantedDotIndicator(
+                    visibleDotCount = 5,
+                    indicatorSize = WantedIndicatorSize.Small,
+                    totalPageCount = 10,
+                    currentIndex = 3
+                )
             }
         }
     }
---
+++ src/main/java/com/wanted/android/wanted/design/pagination/WantedIndicatorContract.kt
@@ -0,0 +1,15 @@
+package com.wanted.android.wanted.design.pagination
+
+object WantedIndicatorContract {
+    enum class WantedIndicatorSize {
+        Small,
+        Normal
+    }
+
+    internal enum class IndicatorDotSize {
+        Max,
+        Mid,
+        Min,
+        Zero
+    }
+}
