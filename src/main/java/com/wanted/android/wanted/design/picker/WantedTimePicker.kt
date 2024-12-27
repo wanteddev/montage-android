14dd5342cf539597cd15e40e5af568c86ed7eecb
danchoo21 <danchoo45@naver.com>
2024-12-27 16:15:01 +0900

    timepicker

--- src/main/java/com/wanted/android/wanted/design/picker/WantedTimePicker.kt
+++ src/main/java/com/wanted/android/wanted/design/picker/WantedTimePicker.kt
@@ -1,26 +1,84 @@
 package com.wanted.android.wanted.design.picker
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccessTime
+import androidx.compose.material.icons.outlined.Keyboard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TimeInput
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.wanted.android.designsystem.R
 import com.wanted.android.wanted.design.DevicePreviews
+import com.wanted.android.wanted.design.base.WantedTouchArea
+import com.wanted.android.wanted.design.button.clickOnceForDesignSystem
 import com.wanted.android.wanted.design.theme.DesignSystemTheme
 import java.util.Calendar
 
-
 @Composable
 fun WantedTimePicker(
+    title: String,
+    confirm: String,
+    cancel: String? = null,
+    onClickConfirm: () -> Unit,
+    onClickCancel: () -> Unit = {},
+    onClickChangeMode: () -> Unit,
+    isEnableClock: Boolean = true,
+    onDismissRequest: () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = { onDismissRequest() },
+    ) {
+        WantedTimePickerContent(
+            modifier = Modifier
+                .clip(RoundedCornerShape(28.dp))
+                .background(colorResource(R.color.background_elevated_normal)),
+            isEnableClock = isEnableClock,
+            title = title,
+            confirm = confirm,
+            cancel = cancel,
+            onClickConfirm = onClickConfirm,
+            onClickCancel = onClickCancel,
+            onClickChangeMode = onClickChangeMode
+        )
+    }
+}
+
+
+@Composable
+private fun WantedTimePickerContent(
     modifier: Modifier = Modifier,
-    isEnableClock: Boolean = true
+    isEnableClock: Boolean = true,
+    title: String,
+    confirm: String,
+    cancel: String? = null,
+    onClickConfirm: () -> Unit,
+    onClickCancel: () -> Unit = {},
+    onClickChangeMode: () -> Unit
 ) {
     val currentTime = Calendar.getInstance()
 
@@ -30,20 +88,176 @@ fun WantedTimePicker(
         is24Hour = false,
     )
 
-    if (isEnableClock) {
-        TimePicker(
-            modifier = modifier,
-            state = timePickerState,
-            colors = TimePickerDefaults.colors()
+    TimePickerLayout(
+        modifier = modifier,
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.labelMedium,
+                color = colorResource(R.color.label_alternative)
+            )
+        },
+        content = {
+            if (isEnableClock) {
+                TimePicker(
+                    state = timePickerState,
+                    colors = getWantedTimePickerDefaults()
+                )
+            } else {
+                TimeInput(
+                    state = timePickerState,
+                    colors = getWantedTimePickerDefaults(
+                        timeSelectorSelectedContainerColor = colorResource(R.color.transparent),
+                        timeSelectorSelectedContentColor = colorResource(R.color.label_normal)
+                    )
+                )
+            }
+        },
+        bottomBar = {
+            TimePickerBottomBar(
+                isEnableClock = isEnableClock,
+                confirm = confirm,
+                cancel = cancel,
+                onClickChangeMode = onClickChangeMode,
+                onClickConfirm = onClickConfirm,
+                onClickCancel = onClickCancel
+            )
+        }
+    )
+}
+
+
+@Composable
+private fun TimePickerBottomBar(
+    modifier: Modifier = Modifier,
+    isEnableClock: Boolean = true,
+    confirm: String,
+    cancel: String?,
+    onClickChangeMode: () -> Unit,
+    onClickConfirm: () -> Unit,
+    onClickCancel: () -> Unit
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(space = 8.dp, alignment = Alignment.End),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        WantedTouchArea(
+            verticalPadding = 4.dp,
+            horizontalPadding = 4.dp,
+            shape = CircleShape,
+            content = {
+                if (isEnableClock) {
+                    Icon(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(8.dp),
+                        imageVector = Icons.Outlined.Keyboard,
+                        tint = colorResource(R.color.label_alternative),
+                        contentDescription = ""
+                    )
+                } else {
+                    Icon(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(8.dp),
+                        imageVector = Icons.Outlined.AccessTime,
+                        tint = colorResource(R.color.label_alternative),
+                        contentDescription = ""
+                    )
+                }
+            },
+            onClick = {
+                onClickChangeMode()
+            }
         )
-    } else {
-        TimeInput(
-            modifier = modifier,
-            state = timePickerState,
-            colors = TimePickerDefaults.colors()
+
+        Spacer(Modifier.weight(1f))
+
+        cancel?.let {
+            Text(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickOnceForDesignSystem { onClickCancel() }
+                    .padding(vertical = 10.dp)
+                    .padding(horizontal = 12.dp),
+                text = cancel,
+                style = MaterialTheme.typography.labelLarge,
+                color = colorResource(R.color.primary_normal)
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .clickOnceForDesignSystem { onClickConfirm() }
+                .padding(vertical = 10.dp)
+                .padding(horizontal = 12.dp),
+            text = confirm,
+            style = MaterialTheme.typography.labelLarge,
+            color = colorResource(R.color.primary_normal)
         )
     }
 }
+
+
+@Composable
+private fun TimePickerLayout(
+    modifier: Modifier = Modifier,
+    title: @Composable () -> Unit,
+    content: @Composable () -> Unit,
+    bottomBar: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .width(IntrinsicSize.Max)
+            .padding(top = 24.dp, bottom = 20.dp)
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+
+        title()
+
+        content()
+
+        bottomBar()
+    }
+}
+
+@Composable
+private fun getWantedTimePickerDefaults(
+    timeSelectorSelectedContainerColor: Color = colorResource(R.color.primary_normal),
+    timeSelectorSelectedContentColor: Color = colorResource(R.color.static_white),
+) = TimePickerDefaults.colors(
+    // 시계 동그란 배경
+    clockDialColor = colorResource(R.color.fill_alternative),
+    // 시계 선택된 숫자 색상
+    clockDialSelectedContentColor = colorResource(R.color.static_white),
+    // 시계 선택 되지 않은 숫자 색상
+    clockDialUnselectedContentColor = colorResource(R.color.label_normal),
+    // 시계 초침
+    selectorColor = colorResource(R.color.primary_normal),
+    // 시간 선택기의 배경 색상.
+    containerColor = colorResource(R.color.background_elevated_normal),
+    // 오전 / 오후 boarder 색상
+    periodSelectorBorderColor = colorResource(R.color.line_normal_normal),
+    // 오전 / 오후 select 배경
+    periodSelectorSelectedContainerColor = colorResource(R.color.primary_normal),
+    //오전 / 오후 unSelect 배경
+    periodSelectorUnselectedContainerColor = colorResource(R.color.background_elevated_normal),
+    // 오전 / 오후 select text
+    periodSelectorSelectedContentColor = colorResource(R.color.static_white),
+    // 오전 / 오후 unselect text
+    periodSelectorUnselectedContentColor = colorResource(R.color.label_alternative),
+    // 시간 선택기의 select 배경
+    timeSelectorSelectedContainerColor = timeSelectorSelectedContainerColor,
+    // 시간 선택기의 unSelect 배경
+    timeSelectorUnselectedContainerColor = colorResource(R.color.fill_normal),
+    // 시간 선택기의 select text
+    timeSelectorSelectedContentColor = timeSelectorSelectedContentColor,
+    // 시간 선택기의 select text
+    timeSelectorUnselectedContentColor = colorResource(R.color.label_normal)
+)
 
 @DevicePreviews
 @Composable
@@ -53,11 +267,35 @@ private fun WantedTimePickerPreview() {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(20.dp),
+                    .padding(20.dp)
+                    .background(colorResource(R.color.background_elevated_normal)),
                 verticalArrangement = Arrangement.spacedBy(20.dp),
             ) {
-                WantedTimePicker(
-                    modifier = Modifier
+
+                WantedTimePickerContent(
+                    isEnableClock = true,
+                    title = "시간선택",
+                    cancel = "취소",
+                    confirm = "확인",
+                    onClickConfirm = {
+                    },
+                    onClickCancel = {
+                    },
+                    onClickChangeMode = {
+                    }
+                )
+
+                WantedTimePickerContent(
+                    isEnableClock = false,
+                    title = "시간선택",
+                    cancel = "취소",
+                    confirm = "확인",
+                    onClickConfirm = {
+                    },
+                    onClickCancel = {
+                    },
+                    onClickChangeMode = {
+                    }
                 )
             }
         }
