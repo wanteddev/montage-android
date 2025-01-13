eb74f78d6b9df3b6a410815bee48ee49ed3a2538
danchoo21 <danchoo45@naver.com>
2025-01-13 14:12:29 +0900

    PI-71341 [Android] Playtime 제작

---
+++ src/main/java/com/wanted/android/wanted/design/playtime/WantedPlayTimeContract.kt
@@ -0,0 +1,12 @@
+package com.wanted.android.wanted.design.playtime
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object WantedPlayTimeContract {
+    enum class Size(val container: Dp, val icon: Dp) {
+        Small(36.dp, 24.dp),
+        Medium(60.dp, 40.dp),
+        Large(80.dp, 56.dp)
+    }
+}
