f6dff7bb83edabeb791d6fa1518ec321459359b8
danchoo21 <danchoo45@naver.com>
2025-01-17 16:45:20 +0900

    PI-71613 [Android] Section Header 제작

---
+++ src/main/java/com/wanted/android/wanted/design/sectionheader/WantedSectionHeader.kt
@@ -0,0 +1,137 @@
+package com.wanted.android.wanted.design.sectionheader
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.wanted.android.designsystem.R
+import com.wanted.android.wanted.design.DevicePreviews
+import com.wanted.android.wanted.design.sectionheader.WantedSectionHeaderContract.Size
+import com.wanted.android.wanted.design.theme.DesignSystemTheme
+import com.wanted.android.wanted.design.util.WantedTextStyle
+
+
+@Composable
+fun WantedSectionHeader(
+    modifier: Modifier,
+    title: String,
+    maxLine: Int = 2,
+    size: Size = Size.Medium,
+    isContentAlignLeft: Boolean = false,
+    textStyle: TextStyle? = null,
+    content: @Composable (RowScope.() -> Unit)? = null
+) {
+
+    val style = textStyle ?: run {
+        WantedTextStyle(
+            colorRes = if (size == Size.XSmall) {
+                R.color.label_alternative
+            } else {
+                R.color.label_strong
+            },
+            style = when (size) {
+                Size.Large -> DesignSystemTheme.typography.title3Bold
+                Size.Medium -> DesignSystemTheme.typography.heading2Bold
+                Size.Small -> DesignSystemTheme.typography.headline2Bold
+                Size.XSmall -> DesignSystemTheme.typography.label1Bold
+            }
+        )
+    }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
+            modifier = Modifier.weight(1f, fill = !isContentAlignLeft),
+            text = title,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = maxLine,
+            style = style
+        )
+
+        Row(
+            modifier = Modifier,
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            content?.invoke(this)
+        }
+    }
+}
+
+
+@DevicePreviews
+@Composable
+private fun WantedSectionHeaderPreview() {
+    DesignSystemTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+            ) {
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목",
+                    content = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목",
+                    content = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목",
+                    isContentAlignLeft = true,
+                    content = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목",
+                    isContentAlignLeft = true,
+                    content = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+            }
+        }
+    }
+}
