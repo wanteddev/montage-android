09a94f53d853ed77657bcbaee0d9e83568e9341d
myseong <myeongyun.seong@wantedlab.com>
2025-01-21 14:17:17 +0900

    PI-71770 [android] TextArea Character Counter 디자인 변경

--- src/main/java/com/wanted/android/wanted/design/textfield/view/WantedTextAreaCharacterCount.kt
+++ src/main/java/com/wanted/android/wanted/design/textfield/view/WantedTextAreaCharacterCount.kt
@@ -6,9 +6,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import com.wanted.android.designsystem.R
 import com.wanted.android.wanted.design.theme.DesignSystemTheme
+import com.wanted.android.wanted.design.util.OPACITY_70
 import com.wanted.android.wanted.design.util.WantedTextStyle
 
 
@@ -23,6 +25,7 @@ internal fun WantedTextAreaCharacterCount(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
+            modifier = Modifier.alpha(OPACITY_70),
             text = "$current",
             style = WantedTextStyle(
                 colorRes = R.color.label_alternative,
@@ -31,9 +34,10 @@ internal fun WantedTextAreaCharacterCount(
         )
 
         Text(
+            modifier = Modifier.alpha(OPACITY_70),
             text = "/$maxWordCount",
             style = WantedTextStyle(
-                colorRes = R.color.label_assistive,
+                colorRes = R.color.label_alternative,
                 style = DesignSystemTheme.typography.label2Medium
             )
         )
