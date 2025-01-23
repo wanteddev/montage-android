683f14a8d978c0e127619e9c3ab2f7e6dbf4c115
myseong <myeongyun.seong@wantedlab.com>
2025-01-23 11:24:17 +0900

    WantedSectionHeader 수정

--- src/main/java/com/wanted/android/wanted/design/sectionheader/WantedSectionHeader.kt
+++ src/main/java/com/wanted/android/wanted/design/sectionheader/WantedSectionHeader.kt
@@ -1,10 +1,12 @@
 package com.wanted.android.wanted.design.sectionheader
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -12,10 +14,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.wanted.android.designsystem.R
 import com.wanted.android.wanted.design.DevicePreviews
 import com.wanted.android.wanted.design.sectionheader.WantedSectionHeaderContract.Size
@@ -28,11 +33,11 @@ fun WantedSectionHeader(
     modifier: Modifier,
     title: String,
     size: Size = Size.Medium,
-    isContentAlignLeft: Boolean = false,
     textStyle: TextStyle? = null,
-    content: @Composable (RowScope.() -> Unit)? = null
+    leftContent: @Composable (() -> Unit)? = null,
+    rightContent: @Composable (RowScope.() -> Unit)? = null
 ) {
-
+    val density = LocalDensity.current
     val style = textStyle ?: run {
         WantedTextStyle(
             colorRes = if (size == Size.XSmall) {
@@ -51,27 +56,51 @@ fun WantedSectionHeader(
 
     Row(
         modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(10.dp)
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(
-            modifier = Modifier.weight(1f, fill = !isContentAlignLeft),
-            text = title,
-            overflow = TextOverflow.Ellipsis,
-            maxLines = if (content != null) 1 else 2,
-            style = style
-        )
-
         Row(
-            modifier = Modifier,
+            modifier = Modifier.weight(1f),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            content?.invoke(this)
+            Text(
+                modifier = Modifier.weight(1f, false),
+                text = title,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = if (leftContent != null) 1 else Int.MAX_VALUE,
+                style = style
+            )
+
+            leftContent?.let {
+                Box(
+                    modifier = Modifier.height(with(density) { style.lineHeight.toDp() }),
+                    contentAlignment = Alignment.Center
+                ) {
+                    leftContent()
+                }
+            }
+        }
+
+        rightContent?.let {
+            Row(
+                modifier = Modifier.height(with(density) { style.lineHeight.toDp() }),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                rightContent.invoke(this)
+            }
         }
     }
 }
 
+@Composable
+fun spToDp(spValue: Float): Dp {
+    val density = LocalDensity.current
+    return with(density) {
+        spValue.sp.toDp()
+    }
+}
 
 @DevicePreviews
 @Composable
@@ -87,7 +116,13 @@ private fun WantedSectionHeaderPreview() {
                 WantedSectionHeader(
                     modifier = Modifier,
                     title = "제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목",
-                    content = {
+                    leftContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    },
+                    rightContent = {
                         Icon(
                             painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
                             contentDescription = ""
@@ -104,7 +139,13 @@ private fun WantedSectionHeaderPreview() {
                 WantedSectionHeader(
                     modifier = Modifier,
                     title = "제목",
-                    content = {
+                    leftContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    },
+                    rightContent = {
                         Icon(
                             painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
                             contentDescription = ""
@@ -115,8 +156,7 @@ private fun WantedSectionHeaderPreview() {
                 WantedSectionHeader(
                     modifier = Modifier,
                     title = "제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목",
-                    isContentAlignLeft = true,
-                    content = {
+                    rightContent = {
                         Icon(
                             painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
                             contentDescription = ""
@@ -127,8 +167,56 @@ private fun WantedSectionHeaderPreview() {
                 WantedSectionHeader(
                     modifier = Modifier,
                     title = "제목",
-                    isContentAlignLeft = true,
-                    content = {
+                    rightContent = {
+                        Icon(
+                            modifier = Modifier,
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목",
+                    size = Size.XSmall,
+                    leftContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목",
+                    size = Size.Small,
+                    leftContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목",
+                    size = Size.Medium,
+                    leftContent = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                WantedSectionHeader(
+                    modifier = Modifier,
+                    title = "제목",
+                    size = Size.Large,
+                    leftContent = {
                         Icon(
                             painter = painterResource(R.drawable.ic_normal_circle_exclamation_fill_svg),
                             contentDescription = ""
