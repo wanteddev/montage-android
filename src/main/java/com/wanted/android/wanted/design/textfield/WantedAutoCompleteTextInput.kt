392394f03d4b460ef1498a4f4e02f435939949ed
myseong <myeongyun.seong@wantedlab.com>
2025-02-13 09:57:48 +0900

    auto complete

--- src/main/java/com/wanted/android/wanted/design/textfield/WantedAutoCompleteTextInput.kt
+++ src/main/java/com/wanted/android/wanted/design/textfield/WantedAutoCompleteTextInput.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wanted.android.designsystem.R
@@ -105,6 +106,101 @@ fun WantedAutoCompleteTextInput(
                 onClickRightButton = onClickRightButton,
                 onValueChange = { text ->
                     isExpended = text.isNotEmpty()
+                    onValueChange(text)
+                },
+            )
+
+            ExposedDropdownMenu(
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(maxWidth),
+                containerColor = colorResource(R.color.background_normal_normal),
+                shape = RoundedCornerShape(20.dp),
+                expanded = isExpended,
+                onDismissRequest = {
+                    isExpended = false
+                }
+            ) {
+                autoCompleteContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun WantedAutoCompleteTextInput(
+    modifier: Modifier = Modifier,
+    value: TextFieldValue,
+    placeholder: String = "",
+    title: String = "",
+    description: String? = null,
+    rightButton: String? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    rightContent: @Composable (() -> Unit)? = null,
+    rightButtonVariant: WantedTextInputRightVariant = WantedTextInputRightVariant.Normal,
+    status: WantedTextInputContract.Status = WantedTextInputContract.Status.Normal,
+    enabled: Boolean = true,
+    rightButtonEnabled: Boolean = true,
+    maxLines: Int = 1,
+    minLines: Int = 1,
+    maxWordCount: Int = 2000,
+    requiredBadge: Boolean = false,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    focused: State<Boolean> = interactionSource.collectIsFocusedAsState(),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    background: Color = colorResource(id = R.color.background_normal_normal),
+    onClickRightButton: () -> Unit = {},
+    onValueChange: (TextFieldValue) -> Unit = {},
+    initExpended: Boolean = false,
+    anchorPadding: Dp = 0.dp,
+    autoCompleteContent: @Composable ColumnScope.() -> Unit
+) {
+    var isExpended by remember { mutableStateOf(initExpended) }
+
+    ExposedDropdownMenuBox(
+        modifier = modifier,
+        expanded = isExpended,
+        onExpandedChange = {
+            isExpended = it
+        }
+    ) {
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            WantedTextInput(
+                modifier = Modifier
+                    .padding(bottom = anchorPadding)
+                    .fillMaxWidth()
+                    .menuAnchor(
+                        type = MenuAnchorType.PrimaryEditable,
+                        enabled = value.text.isNotEmpty()
+                    ),
+                title = title,
+                value = value,
+                description = description,
+                requiredBadge = requiredBadge,
+                status = status,
+                enabled = enabled,
+                rightButtonEnabled = rightButtonEnabled,
+                focused = focused,
+                maxLines = maxLines,
+                minLines = minLines,
+                maxWordCount = maxWordCount,
+                interactionSource = interactionSource,
+                keyboardOptions = keyboardOptions,
+                keyboardActions = keyboardActions,
+                background = background,
+                rightButton = rightButton,
+                rightButtonVariant = rightButtonVariant,
+                placeholder = placeholder,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                rightContent = rightContent,
+                onClickRightButton = onClickRightButton,
+                onValueChange = { text ->
+                    isExpended = text.text.isNotEmpty()
                     onValueChange(text)
                 },
             )
