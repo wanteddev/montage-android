13b9a2799ff49a17e86e9eb9f6b991b58dd4f5c6
myseong <myeongyun.seong@wantedlab.com>
2025-04-30 15:37:48 +0900

    [Android] 디자인 시스템 문서화  - WantedInput 문서화

--- src/main/java/com/wanted/android/wanted/design/input/input/WantedInputContract.kt
+++ src/main/java/com/wanted/android/wanted/design/input/input/WantedInputContract.kt
@@ -1,11 +1,24 @@
 package com.wanted.android.wanted.design.input.input
 
 object WantedInputContract {
+    /**
+     * WantedInput 컴포저블에서 사용할 입력 컴포넌트의 사이즈를 정의합니다.
+     *
+     * Medium - 일반적인 높이의 입력 항목입니다.
+     * Small - 좁은 공간에서 사용할 수 있는 컴팩트한 높이의 입력 항목입니다.
+     */
     enum class WantedInputSize {
         Medium,
         Small
     }
 
+    /**
+     * WantedInput에서 사용할 입력 타입을 정의하는 enum 클래스입니다.
+     *
+     * CheckBox - 일반적인 사각 체크박스입니다.
+     * Radio - 원형의 라디오 버튼 형식입니다.
+     * CheckMark - 체크 아이콘만 표시되는 스타일입니다.
+     */
     enum class WantedInputType {
         CheckBox,
         Radio,
