8396d80a387266db331fb74d762c335752f6c19c
myseong <myeongyun.seong@wantedlab.com>
2025-04-30 13:12:15 +0900

    [Android] 디자인 시스템 문서화  - WantedCell 문서화

--- src/main/java/com/wanted/android/wanted/design/contents/cell/WantedCellContract.kt
+++ src/main/java/com/wanted/android/wanted/design/contents/cell/WantedCellContract.kt
@@ -3,6 +3,19 @@ package com.wanted.android.wanted.design.contents.cell
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+/**
+ * WantedCell의 세로 방향 패딩 크기를 정의하는 enum 클래스입니다.
+ *
+ * 각 값은 셀의 상하 여백을 조정하며, 셀의 전체 높이에 영향을 줍니다.
+ *
+ * 사용 예시 :
+ * ```kotlin
+ * WantedCell(
+ *     text = "텍스트",
+ *     verticalPadding = WantedCellContract.VerticalPadding.Small
+ * )
+ * ```
+ */
 object WantedCellContract {
     enum class VerticalPadding(val value: Dp) {
         None(0.dp),
