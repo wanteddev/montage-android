3bf70b48c1061fe31127303b4151fb575ef4137b
myseong <myeongyun.seong@wantedlab.com>
2025-10-30 11:28:09 +0900

    PI-79473[Android] Top navigation 컴포넌트 업데이트  - floating일때 bottom padding 16dp 추가  - Center 그라데이션 추가

--- src/main/java/com/wanted/android/wanted/design/navigations/topbar/WantedCenterTopAppBar.kt
+++ src/main/java/com/wanted/android/wanted/design/navigations/topbar/WantedCenterTopAppBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -129,7 +131,17 @@ fun WantedCenterTopAppBar(
     }
 
     Box(
-        modifier = modifier.background(background)
+        modifier = if (variant == Variant.Floating) {
+            modifier
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(background, colorResource(R.color.transparent))
+                    )
+                )
+                .padding(bottom = 16.dp)
+        } else {
+            modifier.background(background)
+        }
     ) {
         CompositionLocalProvider(LocalWantedTopBarIconVariant.provides(variant)) {
             when (variant) {
