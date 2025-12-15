ee09e95d178d0c0db21e302a25e27570b177fa2d
danchoo21 <danchoo45@naver.com>
2025-12-15 18:06:24 +0900

    license

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -156,8 +156,8 @@ afterEvaluate {
 
                     licenses {
                         license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
                         }
                     }
 
